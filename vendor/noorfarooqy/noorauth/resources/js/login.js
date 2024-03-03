dd6dfd2363a9d75f9a553edd814b5506d05c32fe
Noor Abdi <mnoor@salaammfbank.co.ke>
2024-03-03 17:17:38 +0300

    remove the vendor and composer lock

--- vendor/noorfarooqy/noorauth/resources/js/login.js
+++
@@ -1,8 +0,0 @@
-import './bootstrap';
-import {createApp} from 'vue/dist/vue.esm-bundler.js';
-import Login from './Components/Login.vue';
-var app = createApp({});
-
-app.component('login-form', Login);
-
-app.mount('#Login');
